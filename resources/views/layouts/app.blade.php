c023ca45febc9cd4e5c0c3424276dc54b2f1817b
YoniBcha <yonasmillion29@example.com>
2025-06-06 15:58:33 +0300

    authentication is added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Customers</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.min.css') }}">
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body>
-    @yield('content');
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100">
+        @include('layouts.navigation')
+
+        <!-- Page Heading -->
+        @isset($header)
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+        @endisset
+
+        <!-- Page Content -->
+        {{-- <main>
+            {{ $slot }}
+        </main> --}}
+        @yield('content');
+
+    </div>
+
     <script src="{{ asset('assets/js/jquery.js') }}"></script>
     <script src="{{ asset('assets/js/bootstrap.bundle.js') }}"></script>
     <script src="{{ asset('assets/js/fontawesome.js') }}"></script>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,34 @@
 <?php
 
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('home');
+});
 
-Route::get('customers/trash', [CustomerController::class, 'trash'])->name('customers.trash');
-Route::patch('customers/restore/{customer}', [CustomerController::class, 'restore'])
-    ->name('customers.restore')
-    ->withTrashed();
-Route::delete('customers/force-delete/{customer}', [CustomerController::class, 'forceDelete'])
-    ->name('customers.force-delete')
-    ->withTrashed();
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('customers', CustomerController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    Route::get('customers/trash', [CustomerController::class, 'trash'])->name('customers.trash');
+    Route::patch('customers/restore/{customer}', [CustomerController::class, 'restore'])
+        ->name('customers.restore')
+        ->withTrashed();
+    Route::delete('customers/force-delete/{customer}', [CustomerController::class, 'forceDelete'])
+        ->name('customers.force-delete')
+        ->withTrashed();
+
+    Route::resource('customers', CustomerController::class);
+});
+
+
+
+require __DIR__ . '/auth.php';
